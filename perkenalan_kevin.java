15bd8476a30f5cfc54e559dea45f726232fe4f7a
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-09-05 11:48:52 +0700

    iki loh rek punyaku

---
+++ perkenalan_kevin.java
@@ -0,0 +1,24 @@
+/**
+ * Tugas04
+ */
+public class Tugas04 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        try {
+            System.out.println("Nama    : Ahmad Kevin Malik Zakaria");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("NIM     : 244107020125");
+        System.out.println("Kelas   : 1D");
+        System.out.println("No Absen: 04");
+        System.out.println("Prodi   : D-IV Teknik INformatika");
+        System.out.println("Alamat  : Jl. Kemirahan");
+        System.out.println("No HP   : 089601302560");
+        System.out.println("Hobi    : Membaca buku");
+    }
+}
