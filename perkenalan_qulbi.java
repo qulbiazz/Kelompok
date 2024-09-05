bc7f841170c6e092ca3816c956fb44eb5099b7a4
qulbyazz <qulbyazzumy@gmail.com>
2024-09-05 11:36:54 +0700

    file qulbi

---
+++ perkenalan_qulbi.java
@@ -0,0 +1,14 @@
+package Kelompok;
+
+class perkenalan_qulbi {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Qulbi Khutsi Azzumi");
+        System.out.println("NIM     : 244107020050");
+        System.out.println("Nama    : TI 1D");
+        System.out.println("Nama    : 23");
+        System.out.println("Nama    : D4 Teknik Informatika");
+        System.out.println("Nama    : Kabupaten Malang");
+        System.out.println("Nama    : 085730013648");
+        System.out.println("Nama    : Desain Garfis");
+    }    
+}
